2d5337ed1498aab3f2f7cdb970ed1db675a0b336
kinppink <648270560@qq.com>
2020-07-01 15:06:21 +0800

    :tada:添加了文章列表页

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,13 @@
+const blogBtnEl = document.querySelector("#blog");
+const contentWrapperEl = document.querySelector(".content-wrapper");
+const headerEl = document.querySelector("header");
+
+blogBtnEl.addEventListener("click", () => {
+    if(!headerEl.classList.contains("side")) {
+        headerEl.classList.add("side");
+        contentWrapperEl.classList.remove("hide");
+    }
+    
+});
+
 
