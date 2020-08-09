0758996b97c5d45ea605d5684e8bbeac8eded6ba
kinppink <648270560@qq.com>
2020-08-09 15:09:41 +0800

    :tada:添加了跨域配置类、MybatisPlus配置类

---
+++ src/main/java/ylchen/blog/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package ylchen.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+//跨域配置
+@Configuration
+public class CorsConfig {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        // 可以自行筛选
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        //corsConfiguration.addExposedHeader("*");
+
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", buildConfig());
+
+        return new CorsFilter(source);
+    }
+}
